e6e31904ccbbba09f1ac50a34468f6b75f68b7af
mischkiey <mischkieycf@gmail.com>
2020-06-05 17:31:54 -0400

    Completed items 1-5

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,17 @@ import item from './item.js';
 import shoppingList from './shopping-list.js';
 
 const main = function () {
-  const itemNames = ['', 'apples', 'pears'];
-  itemNames.forEach(name => {
-    try {
-      item.validateName(name);
-      // create a new item if name is valid
-      store.items.push(item.create(name));
-    } catch (error) {
-      console.log(`Cannot add item: ${error.message}`);
-    }
-  });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
 
 $(main);
+
+store.addItem('bananas');
+store.addItem('apples');
+store.addItem('rice');
+// grab the id of the first store item (bananas)
+let id = store.items[0].id;
+// delete this item from the store
+store.findAndDelete(id);
+shoppingList.render();
--- scripts/store.js
+++ scripts/store.js
@@ -1,11 +1,50 @@
 import item from './item.js';
 
 // const foo = 'bar';
-
+//
 const items = [];
 let hideCheckedItems = false;
 
+const findById = function(id) {
+  return store.items.find(item => item.id === id);
+}
+
+const addItem = function(name) {
+  try {
+    item.validateName(name);
+    const result = item.create(name);
+    this.items.push(result);
+  } catch(error) {
+      console.log(`Cannot add item: ${error.message}`);
+  }
+};
+
+const findAndToggleChecked = function (id) {
+  const result = findById(id);
+  this.items.result.checked = !this.items.result.checked;
+};
+
+const findAndUpdateName = function (id, newName) {
+  try {
+    item.validateName(newName);
+    const result = findById(id);
+    this.items.result.name = newName;
+  } catch(error) {
+      console.log(`Cannot add item: ${error.message}`);
+  }
+}
+
+const findAndDelete = function(id) {
+  const index = this.items.findIndex(item => item.id == id);
+  this.items.splice(i, 1);
+}
+
 export default {
   items,
-  hideCheckedItems
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete,
 };

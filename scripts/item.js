3cf2b42086222aef8740125420ebd5a6708c135b
michellejanosi <michelle.janosi@gmail.com>
2020-06-05 16:06:30 -0400

    add drills

---
+++ scripts/item.js
@@ -0,0 +1,21 @@
+// export default {};
+// const foo = 'bar';
+
+const validateName = function (name) {
+  if (!name) {
+    throw new TypeError('Name must not be blank');
+  }
+};
+
+const create = function (name) {
+  return {
+    id: cuid(),
+    name,
+    checked: false
+  };
+};
+
+export default {
+  validateName,
+  create
+};
---
+++ scripts/store.js
@@ -0,0 +1,11 @@
+import item from './item.js';
+
+// const foo = 'bar';
+
+const items = [];
+let hideCheckedItems = false;
+
+export default {
+  items,
+  hideCheckedItems
+};
